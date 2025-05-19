aea9688d40e62e264ff6c767b50ceb63ec977bb2
alexbiviano <bivianoalex63@gmail.com>
2025-05-19 15:54:18 +0200

    [upd] app.jsx aggiornata

---
+++ react-context-api/src/components/PostsPage.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';  
+import PostsList from './PostsList';  
+
+const PostsPage = () => {  
+    return (  
+        <div>  
+            <h1>Lista dei Post</h1>  
+            <PostsList />  
+        </div>  
+    );  
+};  
+
+export default PostsPage;  
